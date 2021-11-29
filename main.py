db069dea366af80c133f163d042119a1b3ab0ef7
matteonestola <matty.nestola@gmail.com>
2021-11-29 16:54:42 +0100

    upgrade

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import math
 import time
 import numpy as np
 
+import i_dance_move
+import i_finger_face
+import i_front_arms
 import i_one_foot_hand_up
+import i_random_robot
+import i_shuffle
 import i_start_2
 from naoqi import ALProxy
-import m_Sit, m_SitRelax, m_Stand, m_StandInit, m_StandZero, m_Crouch
+import m_Sit, m_SitRelax, m_Stand, m_StandInit, m_StandZero, m_Crouch, m_Wipe_Forehead, m_Hello
 import i_clap, i_disco, i_macarena, i_blow, i_blow_kisses, i_sprinkler, i_the_robot_2, i_GangamStyle, i_thriller, i_arm_dance, i_hips, i_wave_stand_up
 import i_start, i_up_down_hands, i_head_flex, i_ext_clap, i_sit_dance
+import i_waving_pos, i_Right_arm, o_Diagonal_right, o_Diagonal_left,i_Union_arms, o_Arms_opening, i_Move_forward, i_Move_backward, i_Double_movement, o_Rotation_handgun_object, o_Rotation_foot_LLeg, o_Rotation_foot_RLeg
 '''
 import o_Arms_opening, o_Union_arms,o_Right_arm,o_Diagonal_right,o_Diagonal_left,o_Move_forward,o_Move_backward,o_Double_movement,o_Rotation_handgun_object
 import testa_indietro ,testa_avanti,floss_0,floss_1,o_clap,dub_2 , dub
 
 '''
 robotIP = "127.0.0.1"
-port = 44203
+port = 36377
 
-mandatoryPos = [m_StandInit, m_Sit, m_SitRelax, m_Stand, m_StandZero, m_Crouch]
+mandatoryPos = [m_StandInit, m_Sit, m_SitRelax, m_Stand, m_StandZero, m_Wipe_Forehead, m_Hello, m_Crouch]
 intermediatePos = [i_clap, i_disco, i_macarena, i_blow, i_blow_kisses, i_sprinkler, i_the_robot_2, i_GangamStyle, i_thriller, i_arm_dance, i_hips, i_wave_stand_up]
 
 def execute_performance(x):
@@ -89,32 +95,44 @@ def t_def(start, n_value, step):
 def mainFunctionToRun():
     for i in intermediatePos:
 
-        Nao_say(name_pos(str(i)))
+        #Nao_say(name_pos(str(i)))
         execute_performance(i)
-        execute_performance(mandatoryPos[0])
+        #execute_performance(mandatoryPos[0])
 
 
 #mainFunctionToRun()
+
+
 '''
-SEQUENZA BALLO'''
+#SEQUENZA BALLO
 execute_performance(mandatoryPos[0]) # m_StandInit M
+execute_performanceBezier(i_start) #pos_1
 execute_performanceBezier(i_up_down_hands) # pos 2
 execute_performanceBezier(i_head_flex) #pos 3
-execute_performanceBezier(mandatoryPos[4]) # m_StandZero M
+execute_performanceBezier(i_front_arms) # pos front_arms
 execute_performanceBezier(i_ext_clap) # pos 4
 execute_performanceBezier(i_arm_dance) # balla con le mani
 execute_performanceBezier(mandatoryPos[1]) #m_Sit M
 execute_performanceBezier(i_sit_dance) #pos_5
 execute_performanceBezier(mandatoryPos[3]) # m_Stand M
 execute_performanceBezier(i_one_foot_hand_up) # pos_6
-
-
-
-
-
-
-
-
+execute_performanceBezier(i_shuffle) # pos_7
+execute_performanceBezier(i_random_robot) # the robot
+execute_performanceBezier(i_dance_move) # dance move
+execute_performanceBezier(i_finger_face) # pos_8
+execute_performanceBezier(mandatoryPos[2]) # sitRelax M
+execute_performanceBezier(i_waving_pos) # pos_9
+execute_performanceBezier(i_Right_arm) # pos_9
+execute_performanceBezier(mandatoryPos[4]) # m_StandZero M
+execute_performanceBezier(i_Union_arms) # pos_10
+execute_performanceBezier(i_Move_forward) # pos_11
+execute_performanceBezier(i_Move_backward) # pos_12
+execute_performanceBezier(i_Double_movement) # pos_13
+execute_performanceBezier(mandatoryPos[5]) # m_Wipe_ForeHead M
+execute_performanceBezier(mandatoryPos[6]) # m_Hello M
+execute_performanceBezier(mandatoryPos[7]) # m_Crouch M
+'''
+execute_performanceBezier(mandatoryPos[3])
 """
 # copy paste from here to below and leave the two lists below. This code is used to calculate the values of position which are most important from each action file
 import numpy as np
---
+++ positions/i_front_arms.py
@@ -0,0 +1,111 @@
+# Choregraphe bezier export in Python.
+
+names = list()
+times = list()
+keys = list()
+
+names.append("HeadPitch")
+times.append([0.04])
+keys.append([[-0.00043734, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("HeadYaw")
+times.append([0.04])
+keys.append([[0.0033466, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LAnklePitch")
+times.append([0.04])
+keys.append([[-0.00886088, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LAnkleRoll")
+times.append([0.04])
+keys.append([[-0.00768038, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LElbowRoll")
+times.append([0.04])
+keys.append([[-0.0374484, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LElbowYaw")
+times.append([0.04])
+keys.append([[-0.00305847, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LHand")
+times.append([0.04])
+keys.append([[0.00580015, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LHipPitch")
+times.append([0.04])
+keys.append([[-0.00635093, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LHipRoll")
+times.append([0.04])
+keys.append([[0.00574582, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LHipYawPitch")
+times.append([0.04])
+keys.append([[-0.000115165, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LKneePitch")
+times.append([0.04])
+keys.append([[0.00102519, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LShoulderPitch")
+times.append([0.04])
+keys.append([[-0.00648669, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LShoulderRoll")
+times.append([0.04])
+keys.append([[0.0182266, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("LWristYaw")
+times.append([0.04])
+keys.append([[-0.000388482, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RAnklePitch")
+times.append([0.04])
+keys.append([[-0.00886088, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RAnkleRoll")
+times.append([0.04])
+keys.append([[0.00802482, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RElbowRoll")
+times.append([0.04])
+keys.append([[0.0374523, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RElbowYaw")
+times.append([0.04])
+keys.append([[0.00307293, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RHand")
+times.append([0.04])
+keys.append([[0.00580015, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RHipPitch")
+times.append([0.04])
+keys.append([[-0.00635093, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RHipRoll")
+times.append([0.04])
+keys.append([[-0.00574582, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RHipYawPitch")
+times.append([0.04])
+keys.append([[-0.000115165, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RKneePitch")
+times.append([0.04])
+keys.append([[0.00102519, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RShoulderPitch")
+times.append([0.04])
+keys.append([[-0.00340581, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RShoulderRoll")
+times.append([0.04])
+keys.append([[-0.0182518, [3, -0.0133333, 0], [3, 0, 0]]])
+
+names.append("RWristYaw")
+times.append([0.04])
+keys.append([[0.000388482, [3, -0.0133333, 0], [3, 0, 0]]])
+
+
