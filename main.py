dbd98b3208f26b50968436f3f8846cc273647515
matteonestola <matty.nestola@gmail.com>
2021-11-30 14:56:35 +0100

    first 11.30

--- main.py
+++ main.py
@@ -92,6 +92,7 @@ class Move:
 True if Nao standing
 False otherwise
 '''
+'''
 intermediatePos = { 'i_start': Move(True, True, 9.60),
                     'i_up_down_hands': Move(True, True, 5.90),
                     'i_head_flex': Move(True, True, 7.25),
@@ -110,48 +111,112 @@ intermediatePos = { 'i_start': Move(True, True, 9.60),
                     'i_Move_forward': Move(True, True, 2.70),
                     'i_Move_backward': Move(True, True, 2.70),
                     'i_Double_movement': Move(True, True, 6.55)
-}
-#mandatoryPos = [m_StandInit, m_Sit, m_SitRelax, m_Stand, m_StandZero, m_Wipe_Forehead, m_Hello, m_Crouch]
-mandatoryPos = {    'm_StandInit': Move(None, True, 0.05),
-                    'm_Sit': Move(True, False, 9.1),
-                    'm_SitRelax': Move(True, False, 11.48),
-                    'm_Stand': Move(False, True, 11.32),
-                    'm_StandZero': Move(True, True, 8.28),
-                    'm_Wipe_Forehead': Move(None, None, 5.13),
-                    'm_Hello': Move(False, False, 4.66),
-                    'm_Crouch': Move(True, None, 3.05)
-}
+                    }
+# mandatoryPos = [m_StandInit, m_Sit, m_SitRelax, m_Stand, m_StandZero, m_Wipe_Forehead, m_Hello, m_Crouch]
+initialPos = ('m_StandInit', Move(None, True, 0.05))
+mandatoryPos = [
+                ('m_Sit', Move(True, False, 9.1)),
+                ('m_SitRelax', Move(True, False, 11.48)),
+                ('m_Stand', Move(False, True, 11.32)),
+                ('m_StandZero', Move(True, True, 8.28)),
+                ('m_Wipe_Forehead', Move(None, None, 5.13)), # mettere ture al posto di None
+
+                ('m_Hello', Move(False, False, 4.66))
+                ]
+goalPos = ('m_Crouch', Move(True, None, 3.05))
+'''
+intermediatePos = { 'i_start': [True, True, 9.60],
+                    'i_up_down_hands': [True, True, 5.90],
+                    'i_head_flex': [True, True, 7.25],
+                    'i_front_arms': [True, True, 0.05],
+                    'i_ext_clap': [True, True, 5.63],
+                    'i_arm_dance': [True, True, 19.90],
+                    'i_sit_dance': [False, False, 12.04],
+                    'i_one_foot_hand_up': [None, True, 5.90],
+                    'i_shuffle': [True, True, 8.83],
+                    'i_random_robot': [True, True, 6.45],
+                    'i_dance_move': [True, True, 6.02],
+                    'i_finger_face': [True, True, 5.63],
+                    'i_waving_pos': [False, False, 18.55],
+                    'i_Right_arm': [None, None, 9.05],
+                    'i_Union_arms': [None, None, 8.80],
+                    'i_Move_forward': [True, True, 2.70],
+                    'i_Move_backward': [True, True, 2.70],
+                    'i_Double_movement': [True, True, 6.55]
+                    }
+# mandatoryPos = [m_StandInit, m_Sit, m_SitRelax, m_Stand, m_StandZero, m_Wipe_Forehead, m_Hello, m_Crouch]
+initialPos = ('m_StandInit', [None, True, 0.05])
+mandatoryPos = [
+                ('m_Sit', [True, False, 9.1]),
+                ('m_Stand', [False, True, 11.32]),
+                ('m_SitRelax', [True, False, 11.48]),
+                ('m_StandZero', [True, True, 8.28]),
+                ('m_Wipe_Forehead', [None, None, 5.13]), # mettere ture al posto di None
+                ('m_Hello', [False, False, 4.66])
+                ]
+goalPos = ('m_Crouch', [True, None, 3.05])
+totalMandatory = [initialPos, mandatoryPos, goalPos]
+def sum_time_mandatory():
+    sum = 0.0
+    sum += initialPos[1][2]
+    sum += goalPos[1][2]
+    for i in mandatoryPos:
+
+        sum += i[1][2]
+    return sum
+
+
+
+remaining_time = 180.0 - sum_time_mandatory()  # tempo che resta per eseguire le intermediate positions
+print "remaining time fo the execution of intermediate positions:", remaining_time
+intermediate_time = remaining_time / 7
+print(intermediate_time)
+# START PLANNING
+
+for i in range(1, len(totalMandatory)):
+    
+    startPos = totalMandatory[i - 1]  # mossa in cui mi trovo
+    endPos = totalMandatory[i]  # mossa da raggiungere
+    
+
+
+
+
+
+
 
 
 def mainFunctionToRun():
-    #SEQUENZA BALLO
-    execute_performance(mandatoryPos[0]) # m_StandInit M
-    execute_performanceBezier(i_start) #pos_1
-    execute_performanceBezier(i_up_down_hands) # pos 2
-    execute_performanceBezier(i_head_flex) #pos 3
-    execute_performanceBezier(i_front_arms) # pos front_arms
-    execute_performanceBezier(i_ext_clap) # pos 4
-    execute_performanceBezier(i_arm_dance) # balla con le mani
-    execute_performanceBezier(mandatoryPos[1]) #m_Sit M
-    execute_performanceBezier(i_sit_dance) #pos_5
-    execute_performanceBezier(mandatoryPos[3]) # m_Stand M
-    execute_performanceBezier(i_one_foot_hand_up) # pos_6
-    execute_performanceBezier(i_shuffle) # pos_7
-    execute_performanceBezier(i_random_robot) # the robot
-    execute_performanceBezier(i_dance_move) # dance move
-    execute_performanceBezier(i_finger_face) # pos_8
-    execute_performanceBezier(mandatoryPos[2]) # sitRelax M
-    execute_performanceBezier(i_waving_pos) # pos_9
-    execute_performanceBezier(i_Right_arm) # pos_9
-    execute_performanceBezier(mandatoryPos[4]) # m_StandZero M
-    execute_performanceBezier(i_Union_arms) # pos_10
-    execute_performanceBezier(i_Move_forward) # pos_11
-    execute_performanceBezier(i_Move_backward) # pos_12
-    execute_performanceBezier(i_Double_movement) # pos_13
-    execute_performanceBezier(mandatoryPos[5]) # m_Wipe_ForeHead M
-    execute_performanceBezier(mandatoryPos[6]) # m_Hello M
-    execute_performanceBezier(mandatoryPos[7]) # m_Crouch M
+
+    # SEQUENZA BALLO
+    execute_performance(m_StandInit)   # m_StandInit M
+    execute_performanceBezier(i_start)  # pos_1
+    execute_performanceBezier(i_up_down_hands)  # pos 2
+    execute_performanceBezier(i_head_flex)  # pos 3
+    execute_performanceBezier(i_front_arms)  # pos front_arms
+    execute_performanceBezier(i_ext_clap)  # pos 4
+    execute_performanceBezier(i_arm_dance)  # balla con le mani
+    execute_performanceBezier(m_Sit)  # m_Sit M
+    execute_performanceBezier(i_sit_dance)  # pos_5
+    execute_performanceBezier(m_Stand)  # m_Stand M
+    execute_performanceBezier(i_one_foot_hand_up)  # pos_6
+    execute_performanceBezier(i_shuffle)   # pos_7
+    execute_performanceBezier(i_random_robot)   # the robot
+    execute_performanceBezier(i_dance_move)  # dance move
+    execute_performanceBezier(i_finger_face)   # pos_8
+    execute_performanceBezier(m_SitRelax)  # sitRelax M
+    execute_performanceBezier(i_waving_pos)  # pos_9
+    execute_performanceBezier(i_Right_arm)  # pos_9
+    execute_performanceBezier(m_StandZero)  # m_StandZero M
+    execute_performanceBezier(i_Union_arms)  # pos_10
+    execute_performanceBezier(i_Move_forward)  # pos_11
+    execute_performanceBezier(i_Move_backward)  # pos_12
+    execute_performanceBezier(i_Double_movement)  # pos_13
+    execute_performanceBezier(m_Wipe_Forehead)  # m_Wipe_ForeHead M
+    execute_performanceBezier(m_Hello)  # m_Hello M
+    execute_performanceBezier(m_Crouch)  # m_Crouch M
+
 
 #mainFunctionToRun()
 
-execute_performanceBezier(m_Crouch)
+#execute_performanceBezier(m_Crouch)
